5d5843f33dba78070970517f4d900d87b7f76a39
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2026-07-13 21:15:58 +0200

    Remove commented debug logs from gameLogic
    Clean up commented console.log statements from the isWinner function that were left during development.

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -3,10 +3,7 @@ import { state } from "./state.js";
 export const winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 export function isWinner(arr, playerTurn) {
-  // console.log("gameBoard:", state.gameBoard);
-  // console.log("playerTurn:", playerTurn);
   return arr.some((arrVal) => {
-    // console.log(arr, arrVal, playerTurn)
     return arrVal.every((innerVal) => {
       return state.gameBoard[innerVal] === playerTurn;
     })
